36d58c29d9a8633553e769c3463f6ec794951b2e
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-05-13 12:26:22 +0300

    chain - done

---
+++ seminar-011/src/ro/ase/csie/cts/laborator/chain/ModulMesajeGrup.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.laborator.chain;
+
+public class ModulMesajeGrup extends ModulProcesareMesaj {
+    @Override
+    public void procesareMesaj(MesajChat mesaj) {
+        if (mesaj.destinatie.toLowerCase().equals("@everyone")) {
+            System.out.println("Mesaj pentru grup: " + mesaj.text);
+        }
+        if (this.next != null) {
+            this.next.procesareMesaj(mesaj);
+        }
+    }
+}
